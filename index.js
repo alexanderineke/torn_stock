36f72fd87b93441e51d728150dd852c31407b2b9
Appiemonkey <alexander.ineke@futureof.finance>
2022-05-11 08:16:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const axios = require('axios');
+const stocks = [];
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+  // Here is where you need to code
+  if(message.content == "!stocks") {
+     //message.channel.send('heard');
+      axios
+      .get('https://yata.yt/api/v1/travel/export')
+        .then(res => {
+            for(var stock in res.data.stocks) {
+              var b = stock +': \n';
+              for(var stock2 in res.data.stocks[stock].stocks) {
+                var a = res.data.stocks[stock].stocks[stock2];
+                if((a.name.includes('Plushie'))) {
+                 
+                if(a.quantity != 0){
+                  b = b+ a.name + ': ' + a.quantity + '\n';
+                }}
+              }
+              if (b!=stock +': \n'){
+              message.channel.send(b);
+              }
+            }
+        })
+        .catch(error => {
+          console.error(error);
+      });
+    
+  }
+});
+const mySecret = process.env['token'];
+client.login(mySecret);
